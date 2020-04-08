53eb41e2fabcffd56d01c1cb8902e61970c32bbc
NiVanc <nico@izebra.be>
2020-04-08 22:25:58 +0200

    List the servers in the ServerItem component

---
+++ src/components/Server/ServerItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="list-group-item">
+    Server #{{ serverId }}
+    <span :class="{red: serverStatus != 'Normal'}">{{ serverStatus }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["serverId", "serverStatus"]
+};
+</script>
+
+<style scoped>
+span {
+  text-align: right;
+  color: green;
+  float: right;
+}
+
+.red {
+  color: red;
+}
+</style>
--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,18 +1,30 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
-        </ul>
-    </div>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <server-item v-for="server in servers" :serverId="server.id" :serverStatus="server.status"></server-item>
+    </ul>
+  </div>
 </template>
 
 <script>
+import ServerItem from "./ServerItem.vue";
+
+export default {
+  data() {
+    return {
+      servers: [
+        { id: 1, status: "Normal" },
+        { id: 2, status: "Critical" },
+        { id: 3, status: "Unknown" },
+        { id: 4, status: "Normal" }
+      ]
+    };
+  },
+  components: {
+    "server-item": ServerItem
+  }
+};
 </script>
 
 <style>
-
 </style>
